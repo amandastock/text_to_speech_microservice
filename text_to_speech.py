51bd79e824eb1a0fb94c8cb9ac399a52e63416f1
amandastock <80230696+amandastock@users.noreply.github.com>
2022-02-27 18:12:50 -0800

    Add files via upload

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,3 +1,4 @@
+import os
 from gtts import gTTS  # google text to speech conversion module
 import time  # used to delay while loop to check for the file contents, on line 7.
 from os.path import exists as file_exists  # checks if a txt file exists, used on line 5.
@@ -5,16 +6,26 @@ from os.path import exists as file_exists  # checks if a txt file exists, used o
 if file_exists('text_to_read.txt'):  # if the txt file exists
     while True:  # check the file has text and copy it into the variable mytext
         time.sleep(2)  # wait 2 to recheck the file for contents.
-        f = open('text_to_read.txt', 'r')  # open the file
-        mytext = f.read()  # copy the text into mytext variable
-        f.close()  # close the file
+        filesize = os.path.getsize('text_to_read.txt')
+        if filesize == 0:
+            print('The file is empty.')
+        else:
+            # copy the text into mytext variable
+            f = open('text_to_read.txt', 'r+')  # open the file
+            mytext = f.read()
 
-        # passes the text variable and language to gTTS module
-        convert_this = gTTS(text=mytext, lang='en')
+            # passes the text variable and language to gTTS module
+            convert_this = gTTS(text=mytext, lang='en')
 
-        # saves the converted audio in a mp3 file named myaudio.mp3
-        # saves in the same folder as this program on your computer
-        convert_this.save("myaudio.mp3")
+            # saves the converted audio in a mp3 file named audio.mp3
+            # saves in the same folder as this program on your computer
+            convert_this.save("audio.mp3")
+
+            f.truncate(0)
+            f.close()
+            print('Audio file created................')
+else:
+    print('File does not exist.')
 
 
 
